d637f9f64efaa53b3702888a6923c209ed1bd64a
Federico Serron <federico.serron@gmail.com>
2023-07-13 18:54:06 -0300

    Added logs to the start script

--- src/start.sh
+++ src/start.sh
@@ -2,6 +2,27 @@
 
 echo "Worker Initiated"
 
+echo "Directory checks"
+ls -la
+echo "stable-diffusion-webui"
+cd stable-diffusion-webui
+ls -la
+echo "models"
+cd models
+ls -la
+echo "Stable-diffusion"
+cd Stable-diffusion
+ls -la
+echo "Back to root"
+cd /
+ls -la
+echo "runpod-volume"
+cd runpod-volume
+ls -la
+echo "models"
+cd models
+ls -la
+
 echo "Starting WebUI API"
 python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --no-tests --skip-install --lowram --opt-sdp-attention --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check  --no-hashing --no-download-sd-model &
 
